30a5587ab65f264a1bb43b15fd6ab4a0c33968c3
ilviocasa <casamassima.silvio@gmail.com>
2019-10-02 14:56:29 -0300

    init project spring initializr

---
+++ evoluum/src/main/java/com/github/ilviocasa/EvoluumApplication.java
@@ -0,0 +1,13 @@
+package com.github.ilviocasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvoluumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvoluumApplication.class, args);
+	}
+
+}
